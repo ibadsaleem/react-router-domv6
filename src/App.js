bb2d5b8ffb3fb0c10fdac0d23d23f44c90214bb2
ibadsaleem <ibadsaleem@hotmail.com>
2022-11-09 21:12:30 +0500

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import About from "./About";
+import Home from "./Home";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path:"/about",
+      element: <About />
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <div>Home Page</div>;
+  }
+  
+  export default Home;
+  
